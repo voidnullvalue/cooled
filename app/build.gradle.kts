c539a4a4e846d7cc75f8d8d94346ad1cd714f912
|VOID| <rowlandkhd@gmail.com>
2026-05-28 19:04:46 -0500

    Add explicit lifecycle ViewModel Compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ kotlin {
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.0")
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.compose.ui:ui:1.7.8")
     implementation("androidx.compose.ui:ui-tooling-preview:1.7.8")
