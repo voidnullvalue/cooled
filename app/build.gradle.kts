96038c19ed74c427f8ddc906c2f02b9efa66911b
|VOID| <rowlandkhd@gmail.com>
2026-07-05 14:46:09 +0000

    Bump version to 0.5.0 for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.cooled"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.4.0"
+        versionCode = 5
+        versionName = "0.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
